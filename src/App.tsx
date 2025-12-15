2e0e59e731a96b107f7959c386d814d01d92f869
Strelnikov Ilia <ily_strelnikov@mail.ru>
2025-12-15 21:15:51 +0600

    Прописали логику для вывода двух алертов при нажатии на кнопку. А также стейт на отслеживания 4 видов алертов

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 import Modal from "./UI/Modal/Modal.tsx";
+import Alert from "./UI/Alert/Alert.tsx";
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
+
+    const [showAlerts, setShowAlerts] = useState({
+        primary: false,
+        success: false,
+        warning: false,
+        danger: false,
+    });
+
+  const showAlert = (type: 'primary' | 'success' | 'warning' | 'danger') => {
+      setShowAlerts(prev => ({...prev, [type]: true}))
+  };
+
+    const closeAlert = (type: 'primary' | 'success' | 'warning' | 'danger') => {
+        setShowAlerts(prev => ({...prev, [type]: false}))
+    };
+
 
   return (
     <>
@@ -14,9 +31,36 @@ const App = () => {
             >
             Show modal lvl1
         </button>
+
+        <button
+            className='btn btn-secondary m-3'
+            type='button'
+            onClick={() => {
+                showAlert('warning');
+                showAlert('success');
+            }}>
+            Show Alert lvl2
+        </button>
+
+        {showAlerts.warning && (
+            <Alert type="warning" onDismiss={() => closeAlert('warning')}>
+                This is a warning type alert
+            </Alert>
+        )}
+
+        {showAlerts.success && (
+            <Alert type="success">
+                This is a success type alert
+            </Alert>
+        )}
+
         <Modal show={showModal} title={"Custom modal title"}>
             <h3>Modal content</h3>
-            <button className='btn btn-warning' onClick={() => {setShowModal(false)}}>Close modal</button>
+            <button
+                className='btn btn-warning'
+                onClick={() => {setShowModal(false)}}>
+                Close modal
+            </button>
         </Modal>
     </>
   )
--- src/UI/Alert/Alert.tsx
+++ src/UI/Alert/Alert.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 interface Props {
     type: 'primary'| 'success'| 'warning'| 'danger';
     onDismiss?: () => void;
-    children:React.ReactNode;
+    children?:React.ReactNode;
 }
 
 const Alert: React.FC<Props> = ({type, onDismiss, children}) => {
     return (
         <div className={`alert alert-${type}`}>
             {children}
-            {onDismiss ? <button type='button' onClick={onDismiss} className='btn-close float-end'>Close</button> : null}
+            {onDismiss ? <button type='button' onClick={onDismiss} className='btn-close float-end'></button> : null}
         </div>
     );
 };
