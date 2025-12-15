75bfea951717b0b63f46ef30928e72fa7b5c2605
Strelnikov Ilia <ily_strelnikov@mail.ru>
2025-12-15 20:11:30 +0600

    Подготовили компоненет алерт для дальнейшего вывода и реализации

---
+++ src/UI/Alert/Alert.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface Props {
+    type: 'primary'| 'success'| 'warning'| 'danger';
+    onDismiss?: () => void;
+    children:React.ReactNode;
+}
+
+const Alert: React.FC<Props> = ({type, onDismiss, children}) => {
+    return (
+        <div className={`alert alert-${type}`}>
+            {children}
+            {onDismiss ? <button type='button' onClick={onDismiss} className='btn-close float-end'>Close</button> : null}
+        </div>
+    );
+};
+
+export default Alert;
